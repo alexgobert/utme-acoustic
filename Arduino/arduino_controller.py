b80be811197173587156884426c0cad717801186
Alex Gobert <alexandergobert@utexas.edu>
2023-04-03 09:45:55 -0500

    worked on arduino controller. added requirements

--- Arduino/arduino_controller.py
+++ Arduino/arduino_controller.py
@@ -1,15 +1,29 @@
-from ArduinoDAQ import SerialConnect
-from typing import List, Dict
+# from ArduinoDAQ import SerialConnect
+from typing import List, Tuple
+from time import sleep
+from serial import Serial
 
+# default serial settings
 PORT = 'COM7'
 BAUD = 9600
+
 DEGREE_THRESHOLD = 5
 CONTINUOUS_SPEED = 40 # deg/sec
 BUTTON_DELAY = 1
 PLAY_PAUSE = 'p'
 ONE_DEGREE = '1'
 
-def createCommands(angleStep: int, recordDelay: int) -> Dict[str, List]:
+def startRotate(angleStep: int, recordDelay: float, port = PORT, baud = BAUD):
+    arduino = connectToArduino(port, baud)
+    commands = createCommands(angleStep, recordDelay)
+
+    for time, command in commands:
+        sleep(time)
+
+        arduino.write(command.encode())
+        
+
+def createCommands(angleStep: int, recordDelay: float) -> List[Tuple[float, str]]:
     '''
     Creates a list of commands that will be sent to the Arduino.
 
@@ -17,23 +31,18 @@ def createCommands(angleStep: int, recordDelay: int) -> Dict[str, List]:
     ---------
     angleStep : int
         User-defined angle increment
-    recordDelay : int
-        Time to wait before proceeding to next increment
+    recordDelay : float
+        Seconds to wait before proceeding to next increment
 
     Returns
     -------
-    commandDict : dict
-        Dictionary containing command information. Keys are 'time', 'command', and 'value'
-    commandTimes : list
-        List of times to send Arduino commands
-    commandTypes : list
-        List of command keys to send
+    commands : List[Tuple[float, str]]
+        List containing tuples of commands, where the tuple is of the form (sleepTime, command). sleepTime is the time to sleep the current thread before executing its respective command
     '''
 
-    commandTimes = []
-    commandTypes = []
+    commands = []
     
-    if recordDelay < DEGREE_THRESHOLD:
+    if angleStep < DEGREE_THRESHOLD:
         # one degree button angleStep times
         
         pass
@@ -41,9 +50,33 @@ def createCommands(angleStep: int, recordDelay: int) -> Dict[str, List]:
         # play/pause for enough time
         rotationTime = angleStep / CONTINUOUS_SPEED
 
-        for i in range(0, 360 + angleStep, angleStep):
+        # i = angle index
+        for _ in range(0, 360 + angleStep, angleStep):
             # TODO: derive time formula
-            commandTimes.append(i * (1 + rotationTime + recordDelay))
-            commandTypes.append(PLAY_PAUSE)
+
+            commands.append((recordDelay, PLAY_PAUSE)) # start rotation
+            commands.append((rotationTime, PLAY_PAUSE)) # stop rotation
     
-    return {'time': commandTimes, 'command': commandTypes}
+    return commands
+
+def connectToArduino(port: str, baud: int):
+    """
+    Attempts to connect to Arduino through the serial port and sends the acquistion data rate to the Arduino
+    """
+    serialConnection = None
+
+    print(f'Trying to connect to: {port} at {baud} BAUD.')
+    try:
+        serialConnection = Serial(port, baud, timeout=4)
+        print(f'Connected to {port} at {baud} BAUD.')
+    except:
+        print(f'Failed to connect with {port} at {baud} BAUD.')
+
+    if serialConnection:
+        serialConnection.reset_input_buffer()
+
+    return serialConnection
+
+if __name__ == '__main__':
+    commands = createCommands(10, 1)
+    print(commands)
